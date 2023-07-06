be067dc0eae8a3e4def1a54bfc26b72c4ae59ec9
dvcorreia1984 <victor@onsleer.co.za>
2023-07-07 00:32:21 +0200

    Fixed linter errors in styles.css and script.js

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+const modal = document.getElementById('myModal');
+const btn = document.getElementById('openModal');
+const close = document.getElementById('close');
 
-// Get the button that opens the modal
-var btn = document.getElementById("openModal");
+btn.onclick = function btn() {
+  modal.style.display = 'block';
+};
 
-// Get the <span> element that closes the modal
-const close = document.getElementById("close");
+close.onclick = function close() {
+  modal.style.display = 'none';
+};
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-close.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.onclick = function window(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
   }
-}
+};
